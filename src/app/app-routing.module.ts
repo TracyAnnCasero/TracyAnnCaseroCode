4244dfc4c49e1445b414ace8e78e027e02d59786
rence <ftorres@exist.com>
2021-10-20 21:28:18 +0800

    add picture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -100,7 +100,7 @@ const routes: Routes = [
   { path: 'wind', component: SeaComponent },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
   // declarations: [],
   // imports: [
